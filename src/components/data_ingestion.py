8d7d5c612b231d7e77097014c36a37c66e36aaeb
khushal <khushalnegi009@gmail.com>
2026-03-10 16:51:45 +0530

    hyper parameter tunning done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -14,6 +14,9 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+
 @dataclass
 class DataIngestionConfig:
     train_data_path:str=os.path.join('artifacts','train.csv')
@@ -65,4 +68,7 @@ if __name__ == "__main__":
     train_data,test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transormation (train_data,test_data)
+    train_arr,test_arr,_ =  data_transformation.initiate_data_transormation (train_data,test_data)
+
+    modeltrainer = ModelTrainer()
+    modeltrainer.initiate_model_trainer(train_arr,test_arr)
